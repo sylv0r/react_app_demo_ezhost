83ae081610dfde9f8d5cc864f9c40c77446620b5
sylv0r <sylvian.vidal95@gmail.com>
2024-03-07 16:49:46 -0800

    create react app welcome Ez Host

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="p-but-bigger">
+          Welcome to EzHost !!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
